80f58198960ec1b8cb3c74e11ef4c5e22aad8091
trollboss2572 <caspersajewskilee@gmail.com>
2022-10-01 18:11:04 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,27 @@
+import java.io.*;
 public class Driver {
  public static void main(String [] args) {
   Polynomial p = new Polynomial();
   System.out.println(p.evaluate(3));
   double [] c1 = {6,0,0,5};
-  Polynomial p1 = new Polynomial(c1);
+  int [] d1 = {0,3,1,4};
+  Polynomial p1 = new Polynomial(c1, d1);
   double [] c2 = {0,-2,0,0,-9};
-  Polynomial p2 = new Polynomial(c2);
+  int [] d2 = {5, 2, 0, 3, 6};
+  Polynomial p2 = new Polynomial(c2, d2);
+  File file = new File("/Users/caspe/OneDrive/Documents/Shit.txt");
+  Polynomial p3 = new Polynomial (file);
+  for(int i = 0; i < p3.degree.length; i++)
+    System.out.println(p3.poly[i] + ", " + p3.degree[i]);
   Polynomial s = p1.add(p2);
-  System.out.println("s(0.1) = " + s.evaluate(0.1));
+  Polynomial s2 = p1.multiply(p2);
+  System.out.println("p3(2) = " + p3.evaluate(2));
+  System.out.println("s(0.1) = " + s.evaluate(2));
+  System.out.println("p1*p2(1) = " + s2.evaluate(1));
+  p1.saveToFile("/Users/caspe/OneDrive/Documents/Shit.txt");
   if(s.hasRoot(1))
    System.out.println("1 is a root of s");
   else
    System.out.println("1 is not a root of s");
  }
-} 
+}
--- Polynomial.java
+++ Polynomial.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class Polynomial
 {
   double [] poly;
@@ -5,61 +8,162 @@ public class Polynomial
   public Polynomial()
   {
     poly = new double[100];
-    degree = new double[100];
+    degree = new int[100];
     poly[0] = 0;
     degree[0] = 0;
   }
-  public Polynomial(double [] poly_arr; double [] degree_arr)
+  public Polynomial(double [] poly_arr, int [] degree_arr)
   {
     int size = poly_arr.length;
     poly = new double[size];
-    int size2 = degree_arr.length;
+    degree = new int[size];
     for(int i=0; i<size;i++)
     {
-      if (i>=size2)
-
-    }
       poly[i] = poly_arr[i];
-
+      degree[i] = degree_arr[i];
+    }
+  }
+  public Polynomial(File file)
+  {
+    try
+    {
+      Scanner reader = new Scanner(file);
+      String polynomial = reader.nextLine();
+      String[] array_of_polys = polynomial.split("\\+");
+      poly = new double[100];
+      degree = new int[100];
+      int index_place = 1;
+      String double_str = "";
+      int x_checker_index = 1;
+      if (polynomial.charAt(0) == '-')
+      {
+        while(x_checker_index < array_of_polys.length && (Character.isDigit(polynomial.charAt(x_checker_index)) || polynomial.charAt(x_checker_index) == '.'))
+        {
+          double_str = double_str.concat(Character.toString(polynomial.charAt(x_checker_index)));
+          x_checker_index++;
+        }
+        poly[0] = -Double.parseDouble(double_str);
+        if ((x_checker_index < polynomial.length() && polynomial.charAt(x_checker_index) == 'x'))
+          degree[0] = Integer.parseInt(Character.toString(polynomial.charAt(x_checker_index + 1)));
+        else
+          degree[0] = 0;
+      }
+      else
+      {
+        x_checker_index = 0;
+        while(x_checker_index < polynomial.length() && (Character.isDigit(polynomial.charAt(x_checker_index)) || polynomial.charAt(x_checker_index) == '.'))
+        {
+          double_str = double_str.concat(Character.toString(polynomial.charAt(x_checker_index)));
+          x_checker_index++;
+        }
+        poly[0] = Double.parseDouble(double_str);
+        if ((x_checker_index < polynomial.length() && polynomial.charAt(x_checker_index) == 'x'))
+          degree[0] = Integer.parseInt(Character.toString(polynomial.charAt(x_checker_index + 1)));
+        else
+          degree[0] = 0;
+      }
+      for(int i = 1; i < array_of_polys.length; i++)
+      {
+        double_str = "";
+        x_checker_index = 0;
+        while(x_checker_index < array_of_polys[i].length() && (Character.isDigit(array_of_polys[i].charAt(x_checker_index)) || array_of_polys[i].charAt(x_checker_index) == '.'))
+        {
+          double_str = double_str.concat(Character.toString(array_of_polys[i].charAt(x_checker_index)));
+          x_checker_index++;
+        }
+        poly[index_place] = Double.parseDouble(double_str);
+        if ((x_checker_index < array_of_polys[i].length() && array_of_polys[i].charAt(x_checker_index) == 'x'))
+          degree[index_place] = Integer.parseInt(Character.toString(array_of_polys[i].charAt(x_checker_index + 1)));
+        else
+          degree[index_place] = 0;
+        index_place++;
+      }
+      array_of_polys = polynomial.split("\\-");
+      for(int i = 1; i < array_of_polys.length; i++)
+      {
+        x_checker_index = 0;
+        double_str = "";
+        while((x_checker_index < array_of_polys[i].length()) && (Character.isDigit(array_of_polys[i].charAt(x_checker_index)) || array_of_polys[i].charAt(x_checker_index) == '.'))
+        {
+          double_str = double_str.concat(Character.toString(array_of_polys[i].charAt(x_checker_index)));
+          x_checker_index++;
+        }
+        poly[index_place] = -Double.parseDouble(double_str);
+        if ((x_checker_index < array_of_polys[i].length() && array_of_polys[i].charAt(x_checker_index) == 'x'))
+          degree[index_place] = Integer.parseInt(Character.toString(array_of_polys[i].charAt(x_checker_index + 1)));
+        else
+          degree[index_place] = 0;
+        index_place++;
+      }
+    }
+    catch (FileNotFoundException e)
+    {
+      e.printStackTrace();
+    }
   }
   public Polynomial add(Polynomial second_poly)
   {
-    int size = Math.max(second_poly.poly.length, poly.length);
+    int size = second_poly.poly.length + poly.length;
     double [] updated_size = new double[size];
+    int [] updated_exp = new int[size];
+    int index_of_0 = 0;
     if (poly.length <= size)
     {
-      int UwU = poly.length;
-      for(int i = 0; i < size; i++)
+      for(int i = 0; i < poly.length; i++)
       {
-        if (i < UwU)
-          updated_size[i] = poly[i];
-        else
-          updated_size[i] = 0;
+        for(int x = 0; x < second_poly.poly.length; x++)
+        {
+          if(degree[i] == second_poly.degree[x])
+          {
+            updated_size[index_of_0] = poly[i] + second_poly.poly[x];
+            updated_exp[index_of_0] = degree[i];
+            index_of_0++;
+            break;
+          }
+        }
       }
     }
-    else
+    for(int i = 0; i < second_poly.degree.length; i++)
     {
-      int UwU = second_poly.poly.length;
-      for(int i = 0; i < size; i++)
+      boolean already_summed = false;
+      for(int x: updated_exp)
+        if (second_poly.degree[i] == x)
+        {
+            already_summed = true;
+            break;
+        }
+      if (!already_summed)
       {
-        if (i < UwU)
-          updated_size[i] = second_poly.poly[i];
-        else
-          updated_size[i] = 0;
+        updated_size[index_of_0] = second_poly.poly[i];
+        updated_exp[index_of_0] = second_poly.degree[i];
+        index_of_0++;
       }
     }
-    double [] new_poly = new double[size];
-    for(int i=0; i<size; i++)
-      new_poly[i] = updated_size[i] + second_poly.poly[i];
-    Polynomial return_poly = new Polynomial(new_poly);
-    return (return_poly);
+    for(int i = 0; i < degree.length; i++)
+    {
+      boolean already_summed = false;
+      for(int x: updated_exp)
+        if (degree[i] == x)
+        {
+            already_summed = true;
+            break;
+        }
+      if (!already_summed)
+      {
+        updated_size[index_of_0] = poly[i];
+        updated_exp[index_of_0] = degree[i];
+        index_of_0++;
+      }
+    }
+    Polynomial return_poly = new Polynomial(updated_size, updated_exp);
+    return return_poly;
   }
   public double evaluate(double val_for_x)
   {
     int size = poly.length;
     double sum = 0;
     for (int i=0; i<size; i++)
-      sum += (poly[i]*Math.pow(val_for_x, i));
+      sum += (poly[i]*Math.pow(val_for_x, degree[i]));
       return (sum);
   }
   public boolean hasRoot(double x)
@@ -68,5 +172,63 @@ public class Polynomial
       return (true);
     else
       return (false);
+  }
+  public Polynomial multiply(Polynomial p2)
+  {
+    int size = p2.poly.length * poly.length;
+    double [] result_poly = new double[size];
+    int [] result_degree = new int[size];
+    int index_of_0 = 0;
+    for(int i = 0; i < poly.length; i++)
+    {
+      for(int x = 0; x < p2.poly.length; x++)
+      {
+        result_poly[index_of_0] = poly[i] * p2.poly[x];
+        result_degree[index_of_0] = degree[i] + p2.degree[x];
+        index_of_0++;
+      }
+    }
+    for(int i = 0; i < result_degree.length; i++)
+    {
+      for(int x = 0; x < result_degree.length; x++)
+      {
+        if (result_degree[i] == result_degree[x] && x != i)
+        {
+          result_poly[i] = result_poly[i] + result_poly[x];
+          result_poly[x] = 0;
+        }
+      }
+    }
+    Polynomial return_poly = new Polynomial(result_poly, result_degree);
+    return (return_poly);
+  }
+  public void saveToFile(String filepath)
+  {
+    try
+    {
+      String poly_as_string = "";
+      if (poly[0] != 0)
+        poly_as_string = poly_as_string.concat(Double.toString(poly[0]));
+      for(int i = 1; i < poly.length; i++)
+      {
+        if (poly[i] > 0)
+          poly_as_string = poly_as_string.concat("+");
+        else if (poly[i] == 0)
+          continue;
+        poly_as_string = poly_as_string.concat(Double.toString(poly[i]));
+        if (degree[i] != 0)
+        {
+          poly_as_string = poly_as_string.concat("x");
+          poly_as_string = poly_as_string.concat(Integer.toString(degree[i]));
+        }
+      }
+      FileWriter file = new FileWriter(filepath, false);
+      file.write(poly_as_string);
+      file.close();
+    }
+    catch (IOException e)
+    {
+      System.out.println("fuckey wuckey");
+    }
   }
 }
